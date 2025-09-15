54df07bd28788898cdd68c20744eaabd1c1ec78f
Moisés <69405573+moisesEL@users.noreply.github.com>
2025-09-15 18:53:33 +0000

    modificaciones

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 
+/* Codigo anterior */
+/*
 for (let a = 0; a < pronoun.length; a++) {
   for (let b = 0; b < adj.length; b++) {
     for (let d = 0; d < noun.length; d++) {
       console.log(pronoun[a] + adj[b] + noun[d] + '.com');
-      
     }
   }
-}
+} */
+
+  pronoun.forEach(a => {
+  adj.forEach(b => {
+    noun.forEach (c => {
+     console.log(a + b + c + '.com')
+  })})}); 
+   /* Utilice forEachh para anidar los arrays*/
